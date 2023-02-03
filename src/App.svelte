cc9a3cabac2e4fb6f5d108a65d0c3bacbd9bf6cb
thepiguy <kunal.a.dandekar@gmail.com>
2023-02-03 15:52:30 +0100

    css

--- src/App.svelte
+++ src/App.svelte
@@ -10,13 +10,10 @@
   ];
 </script>
 
+<div id="overlay" />
 <main>
   <h1 class="container">
-    <p id="heading" class="glitch">
-      <span aria-hidden="true">codepen</span>
-      codepen
-      <span aria-hidden="true">codepen</span>
-    </p>
+    <p id="heading" class="glitch">Highscores</p>
   </h1>
   <div class="container scores no-scrollbar">
     <div class="top first">
@@ -35,6 +32,15 @@
 </main>
 
 <style>
+  #overlay {
+    box-shadow: inset 0 0 100px rgb(31, 33, 24);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+
   .container.scores {
     height: 100%;
     width: 100%;
@@ -52,26 +58,20 @@
   .name {
     font-family: "Audiowide";
     text-transform: uppercase;
+    font-size: 1.5rem;
   }
 
-  .top:nth-child(1) {
-    font-size: 1.7rem;
+  .top {
+    line-height: 0 !important;
   }
-  .top:nth-child(2) {
-    font-size: 1.3rem;
-  }
-  .top:nth-child(3) {
-    font-size: 1.1rem;
-  }
-
 
   h1.container {
     align-items: center;
   }
 
-
   .glitch {
-    font-size: 2rem;
+    color: #fffc00;
+    font-size: 1.7rem;
     position: absolute;
     font-weight: bold;
     text-transform: uppercase;
@@ -79,26 +79,6 @@
     text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
       0.025em 0.04em 0 #fffc00;
     animation: glitch 725ms infinite;
-  }
-
-  .glitch span {
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-
-  .glitch span:first-child {
-    animation: glitch 500ms infinite;
-    clip-path: polygon(0 0, 100% 0, 100% 35%, 0 35%);
-    transform: translate(-0.04em, -0.03em);
-    opacity: 0.75;
-  }
-
-  .glitch span:last-child {
-    animation: glitch 375ms infinite;
-    clip-path: polygon(0 65%, 100% 65%, 100% 100%, 0 100%);
-    transform: translate(0.04em, 0.03em);
-    opacity: 0.75;
   }
 
   @keyframes glitch {
